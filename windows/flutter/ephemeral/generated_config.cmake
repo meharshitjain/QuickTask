a15adcd607cdd031e4b9dd6878373a06b3a6f2aa
Harshit Jain <jharshit1997@gmail.com>
2024-11-14 20:52:38 +0530

    Added rest other boilerplate code.

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Tools\\flutter"
+  "PROJECT_DIR=E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task"
+  "FLUTTER_ROOT=D:\\Tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task"
+  "FLUTTER_TARGET=E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\BITS\\BITS- Semester 3 (24H2)\\Cross Platform Application Development (Merged) (Regular)\\Exam Papers\\Assignment\\QuickTask\\quick_task\\.dart_tool\\package_config.json"
+)
